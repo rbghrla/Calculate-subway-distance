578905ec2401631f7b88b2dfec8fad893e2a98fd
ghkim <rbghrla@naver.com>
2022-05-22 19:50:40 +0900

    make LineRepository Use ArrayList.

---
+++ src/main/java/subway/domain/line/LineRepository.java
@@ -0,0 +1,41 @@
+package subway.domain.line;
+
+import subway.exception.NoSuchLineException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class LineRepository {
+    private static final List<Line> lines = new ArrayList<>();
+
+    static {
+        lines.add(new Line("2호선"));
+        lines.add(new Line("3호선"));
+        lines.add(new Line("신분당선"));
+    }
+
+    public static List<Line> lines() {
+        return Collections.unmodifiableList(lines);
+    }
+
+    public static void addLine(Line line) {
+        lines.add(line);
+    }
+
+    public static boolean deleteLineByName(String name) {
+        return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static void deleteAll() {
+        lines.clear();
+    }
+
+    public static Line findLineByName(String name) {
+        return lines.stream()
+                .filter(line -> line.getName().equals(name))
+                .findFirst()
+                .orElseThrow(NoSuchLineException::new);
+    }
+}
