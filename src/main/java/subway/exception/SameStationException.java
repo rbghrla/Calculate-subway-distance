78b133852ee0c7c6a71a3bef882812d3a86eba76
ghkim <rbghrla@naver.com>
2022-05-23 21:13:23 +0900

    Delete useless class

--- src/main/java/subway/exception/SameStationException.java
+++
@@ -1,9 +0,0 @@
-package subway.exception;
-
-public class SameStationException extends RuntimeException {
-    private static final String ERROR_MESSAGE = "[ERROR] 출발역과 도착역은 동일하면 안됩니다.";
-
-    public SameStationException() {
-        super(ERROR_MESSAGE);
-    }
-}
