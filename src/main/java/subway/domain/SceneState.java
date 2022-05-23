8348f6dca74b891195713b350a7dd353c4bae34c
ghkim <rbghrla@naver.com>
2022-05-23 21:12:47 +0900

    finish to make SceneState

--- src/main/java/subway/domain/SceneState.java
+++ src/main/java/subway/domain/SceneState.java
@@ -10,3 +10,17 @@ public enum SceneState {
     DISTANCE_MIN_SCENE("PATH_SCENE_1"),
     TIME_MIN_SCENE("PATH_SCENE_2"),
     QUIT("MAIN_SCENE_q");
+
+    private String value;
+
+    SceneState(String value) {
+        this.value = value.toLowerCase();
+    }
+
+    public static SceneState findSceneStateByInput(String input) {
+        return Arrays.stream(SceneState.values())
+                .filter(sceneState -> sceneState.value.equals(input))
+                .findFirst()
+                .orElseThrow(NoSuchChoiceException::new);
+    }
+}
